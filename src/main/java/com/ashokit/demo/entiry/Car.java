d1fc66defb9cfd399a9c45589f3273db12da20ee
nandni <nandni@gmail.com>
2022-08-04 11:10:29 +0530

    main class is modified

---
+++ src/main/java/com/ashokit/demo/entiry/Car.java
@@ -0,0 +1,12 @@
+package com.ashokit.demo.entiry;
+
+public class Car {
+	
+	public void start()
+	{
+		//changes for HIS-122 Im middle of this correction
+		//12.30pm my Tl give me bug fix HIS-134
+		//then i m confuse can i commite or leave
+	}
+
+}
