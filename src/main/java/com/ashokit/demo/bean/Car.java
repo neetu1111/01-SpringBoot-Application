3bcb4ce26de794bb674894dd7f4e2fd7ca4fa8c2
nandni <nandni@gmail.com>
2022-08-04 12:30:07 +0530

    commit after stash

--- src/main/java/com/ashokit/demo/bean/Car.java
+++ src/main/java/com/ashokit/demo/bean/Car.java
@@ -1,5 +1,19 @@
 package com.ashokit.demo.bean;
 
 public class Car {
+	
+	public void start()
+	{
+		//changes for HIS-122 i m doing middle of that
+		//now my TL assign very important bug fixing work HIS-134
+		//what i do can i commit HIS-122 or LEAVE it as it is
+		
+		
+		//what i do??
+		
+		//then use git stash command 
+		
+		int i=100;
+	}
 
 }
